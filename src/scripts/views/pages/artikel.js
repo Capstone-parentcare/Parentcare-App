552f50aa18ea63fd6b36903ac1ed82b03fd5f636
RaidyMR <raidymustafid39@gmail.com>
2022-11-30 23:07:54 +0700

    add mobile responsive in article page

--- src/scripts/views/pages/artikel.js
+++ src/scripts/views/pages/artikel.js
@@ -4,24 +4,24 @@ import { createMovieItemTemplate } from '../templates/template-creator';
 const Upcoming = {
   async render() {
     return `
-      <span class="judul">Artikel List</span>
-      
       <div class="artikel-container">
+        <span class="judul"><h1>Artikel List</h1></span>
         <div class="artikel">
-          <div><img src="../../image/article_image_1.jpg" alt="Gambar artikel"></div>
-          <div><a href="#/detail1">Tips Mendampingi Anak Mengerjakan PR</a></div>
+          <img src="../../image/article_image_1.jpg" class="gambar-artikel" alt="Gambar artikel">
+          <p>
+            <a href="#/detail1">Tips Mendampingi Anak Mengerjakan PR</a>
+          </p>
         </div>
-        <div class="garis"></div>
-      </div>
+        <hr>
 
-      <div class="artikel-container">
         <div class="artikel">
-          <div><img src="../../image/article_image_1.jpg" alt="Gambar artikel"></div>
-          <div><a href="/">Tips Mendampingi Anak Mengerjakan PR</a></div>
+          <img src="../../image/article_image_1.jpg" class="gambar-artikel" alt="Gambar artikel">
+          <p>
+            <a href="#/detail1">Tips Mendampingi Anak Mengerjakan PR</a>
+          </p>
         </div>
-        <div class="garis"></div>
+        <hr>
       </div>
-
     `;
   },
 
