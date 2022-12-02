570c86142af3bf09da4dafbf52fa7eeb6b7c98c6
RaidyMR <raidymustafid39@gmail.com>
2022-12-02 11:25:44 +0700

    add article-detail and answer-detail endpoint

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -4,7 +4,9 @@ const API_ENDPOINT = {
   QUESTION: `${CONFIG.BASE_URL}questions/`,
   QUESTION_DETAIL: (id) => `${CONFIG.BASE_URL}questions/${id}`,
   ARTICLE: `${CONFIG.BASE_URL}articles/`,
+  ARTICLE_DETAIL: (id) => `${CONFIG.BASE_URL}articles/${id}`,
   ANSWER: `${CONFIG.BASE_URL}answers/`,
+  ANSWER_DETAIL: (id) => `${CONFIG.BASE_URL}answers/${id}`,
 };
 
 export default API_ENDPOINT;
