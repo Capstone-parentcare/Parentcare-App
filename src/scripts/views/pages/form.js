1b23a3501594a94143aee2406c6ccdd59093949a
RaidyMR <raidymustafid39@gmail.com>
2022-12-02 11:59:59 +0700

    mobile responsive on question form

--- src/scripts/views/pages/form.js
+++ src/scripts/views/pages/form.js
@@ -9,21 +9,15 @@ const Like = {
       <form id="question-form" method="POST" action="${API_ENDPOINT.QUESTION}">
         <div>
           <label for="writer">Nama:</label><br>
-          <input type="text" id="writer" name ="writer" placeholder="nama" value="
-          bapak anonim
-          "><br>
+          <input type="text" id="writer" name ="writer" placeholder="nama" required><br>
         </div>
         <div>
           <label for="title">Judul:</label><br>
-          <input type="text" id="title" name="title" placeholder="Judul Pertanyaan" value="
-            QUESTION TITLE
-          "><br>
+          <input type="text" id="title" name="title" placeholder="Judul Pertanyaan" required><br>
         </div>
         <div>
           <label for="content">Pertanyaan:</label><br>
-          <textarea id="content" name="content" placeholder="Isi Pertanyaan" value="
-          Vestibulum urna est, dignissim non quam volutpat, commodo rutrum magna. Cras placerat velit in quam porta, vel sollicitudin urna gravida. Etiam pharetra nec tortor eu pellentesque. Vivamus viverra fermentum velit in condimentum. Suspendisse sed lorem sit amet massa interdum imperdiet. Duis luctus sodales congue. Donec vestibulum velit sit amet consectetur euismod. 
-          "></textarea><br>        
+          <textarea id="content" name="content" placeholder="Isi Pertanyaan" required></textarea><br>        
         </div>
         <input type="submit" value="Submit">
       </form> 
