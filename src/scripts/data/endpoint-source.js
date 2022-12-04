42ed3c0326fbd786d0b212ec55c1156ca1336140
RaidyMR <raidymustafid39@gmail.com>
2022-12-05 01:15:19 +0700

    delete console.log(data)

--- src/scripts/data/endpoint-source.js
+++ src/scripts/data/endpoint-source.js
@@ -4,35 +4,35 @@ class EndpointSource {
   static async questionList() {
     const response = await fetch(API_ENDPOINT.QUESTION);
     const responseJson = await response.json();
-    console.log(responseJson.data);
+
     return responseJson.data;
   }
 
   static async questionDetail(id) {
     const response = await fetch(API_ENDPOINT.QUESTION_DETAIL(id));
     const responseJson = await response.json();
-    console.log(responseJson.data);
+
     return responseJson.data;
   }
 
   static async questionDetailWithAnswer(id) {
     const response = await fetch(API_ENDPOINT.ANSWER_BY_QUESTION_ID(id));
     const responseJson = await response.json();
-    console.log(responseJson.data);
+
     return responseJson.data;
   }
 
   static async articleList() {
     const response = await fetch(API_ENDPOINT.ARTICLE);
     const responseJson = await response.json();
-    console.log(responseJson.data);
+
     return responseJson.data;
   }
 
   static async articleDetail(id) {
     const response = await fetch(API_ENDPOINT.ARTICLE_DETAIL(id));
     const responseJson = await response.json();
-    console.log(responseJson.data);
+
     return responseJson.data;
   }
 }
--- src/scripts/utils/form-initiator.js
+++ src/scripts/utils/form-initiator.js
@@ -2,7 +2,6 @@ import questionFormHandler from './question-form-handler';
 
 const QuestionFormInitiator = {
   init({ formContainer }) {
-    console.log('init form success');
     formContainer.addEventListener('submit', (event) => {
       questionFormHandler(event);
     });
