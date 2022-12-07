d5a266c6335a5cfc90da95668f6519f7c2f966ac
IlmanNawali <79490380+IlmanNawali@users.noreply.github.com>
2022-12-07 17:38:41 +0700

    Add files via upload

---
+++ src/scripts/utils/login-form-handler.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+const axios = require('axios');
+const Swal = require('sweetalert2');
+
+const questionFormHandler = async (event) => {
+  event.preventDefault();
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+  axios.post(`http://localhost:8081/api/login?email=${email}&password=${password}`).then((response) => {
+    console.log('HIHI', response.data.status);
+    if (response.data.status === 'Success') {
+      console.log('MASUKK');
+      window.location.href = '#/questions';
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Password Atau Email Salah',
+        text: response.data.display_message,
+      });
+    }
+  });
+};
+
+export default questionFormHandler;
--- src/scripts/views/pages/login.js
+++ src/scripts/views/pages/login.js
@@ -1,23 +1,26 @@
 import API_ENDPOINT from '../../globals/api-endpoint';
+import LoginFormInitiator from '../../utils/login-form-initiator';
 
 const Login = {
   async render() {
     return `
     <div class="login-container">
         <h1>Login</h1>
-        <form>
-            <label>Username</label><br>
-            <input type="text" placeholder="Username"><br>
+        <form id="login-form" method="POST" action="${API_ENDPOINT.LOGIN}">
+            <label>Email</label><br>
+            <input type="email" id="email" name ="email" placeholder="Isi Email Anda" required><br>
             <label>Password</label><br>
-            <input type="password" placeholder="Password"><br>
-            <button>Log in</button>
+            <input type="password" id="password" name="password" placeholder="Isi Password Anda" required><br>
+            <input type="submit" value="Submit">
         </form>
       </div>
     `;
   },
 
   async afterRender() {
-    
+    LoginFormInitiator.init({
+      formContainer: document.querySelector('#login-form'),
+    });
   },
 };
 
