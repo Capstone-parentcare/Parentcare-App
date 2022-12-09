77ae0f27987319804549231845bf53d4183605ed
RaidyMR <raidymustafid39@gmail.com>
2022-12-09 17:46:38 +0700

    refactoring article form handler

--- src/scripts/utils/article-form-handler.js
+++ src/scripts/utils/article-form-handler.js
@@ -1,4 +1,4 @@
-// import API_ENDPOINT from '../globals/api-endpoint';
+import API_ENDPOINT from '../globals/api-endpoint';
 
 const axios = require('axios');
 
@@ -6,34 +6,24 @@ const articleFormHandler = async (event) => {
   event.preventDefault();
   const data = new FormData();
   const doctorid = document.querySelector('#doctor_id');
-  const tit = document.querySelector('#title');
-  const con = document.querySelector('#content');
-  const img = document.querySelector('#image');
+  const title = document.querySelector('#title');
+  const content = document.querySelector('#content');
+  const image = document.querySelector('#image');
 
-  // const doctor_id = data.append("doctor_id", doctorid);
-  // const title = data.append("title", tit);
-  // const content = data.append("content", con);
-  // const image = data.append("image", img.files[0].value);
-  // console.log("cekcek1", doctor_id);
-  // console.log("cekcek2", title);
-  // console.log("cekcek3", content);
-  // console.log("cekcek3", image);
   data.append('doctor_id', doctorid.value);
-  data.append('title', tit.value);
-  data.append('content', con.value);
-  data.append('image', img.files[0]);
-  console.log('data', data);
+  data.append('title', title.value);
+  data.append('content', content.value);
+  data.append('image', image.files[0]);
 
   const config = {
-    method: 'post',
-    url: 'http://localhost:8081/api/articles/',
+    method: 'POST',
+    url: API_ENDPOINT.ARTICLE,
     data,
   };
   axios(config)
     .then((response) => {
       console.log(JSON.stringify(response.data));
-      alert('data berhasil masuk');
-      location.replace('#/articles_psychologist');
+      location.replace('#/articles');
     })
     .catch((error) => {
       console.log(error);
