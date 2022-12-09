430d67c92032c2d2e367f30238ebd8542c982c2b
RaidyMR <raidymustafid39@gmail.com>
2022-12-09 15:57:52 +0700

    update url routes

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -5,7 +5,6 @@ import QuestionForm from '../views/pages/question-form';
 import QuestionDetail from '../views/pages/question-detail';
 import Login from '../views/pages/login';
 import HomePsychologist from '../views/pages/home-psychologist';
-import ArticlePsychologist from '../views/pages/article-psychologist';
 import ArticleForm from '../views/pages/article-form';
 import Answer from '../views/pages/answer';
 import AnswerPyscho from '../views/pages/question-detail-pyschologist';
@@ -14,15 +13,13 @@ const routes = {
   '/': Home, // default page
   '/questions': HomePsychologist,
   '/questions/:id': QuestionDetail,
-  '/form': QuestionForm,
+  '/questions_form': QuestionForm,
   '/articles': Article,
   '/articles/:id': ArticleDetail,
-  '/login': Login,
-  // '/questions_psychologist': HomePsychologist,
-  '/articles_psychologist': ArticlePsychologist,
   '/articles_form': ArticleForm,
-  '/answer/:id': Answer,
-  '/answer_detail_psycho/:id': AnswerPyscho,
+  '/answers/:id': AnswerPyscho,
+  '/answers_form/:id': Answer,
+  '/login': Login,
 };
 
 export default routes;
